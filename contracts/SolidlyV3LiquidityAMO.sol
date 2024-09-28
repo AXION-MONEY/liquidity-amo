d6835376c605cfc3503bdcaf2081ee0294831cc2
MiKO <39623615+MiKoronjoo@users.noreply.github.com>
2024-09-28 10:42:00 +0330

    Fix boost price calculation bug for SolidlyV3LiquidityAMO

--- contracts/SolidlyV3LiquidityAMO.sol
+++ contracts/SolidlyV3LiquidityAMO.sol
@@ -583,7 +583,11 @@ contract SolidlyV3LiquidityAMO is
         if (boost < usd) {
             price = (10 ** (boostDecimals - usdDecimals + PRICE_DECIMALS) * sqrtPriceX96 ** 2) / Q96 ** 2;
         } else {
-            price = ((sqrtPriceX96 ** 2 / Q96 ** 2) * 10 ** PRICE_DECIMALS) / 10 ** (boostDecimals - usdDecimals);
+            if (sqrtPriceX96 >= Q96) {
+                price = 10 ** (boostDecimals - usdDecimals + PRICE_DECIMALS) / (sqrtPriceX96 ** 2 / Q96 ** 2);
+            } else {
+                price = (10 ** (boostDecimals - usdDecimals + PRICE_DECIMALS) * Q96 ** 2) / sqrtPriceX96 ** 2;
+            }
         }
     }
 }
