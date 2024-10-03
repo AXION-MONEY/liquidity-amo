01ff69090796ca7ba28ee336f361457b3a848c90
mz8t <125902611+mz8t@users.noreply.github.com>
2024-10-03 11:50:58 +0200

    Update SolidlyV2AMO.sol

--- contracts/SolidlyV2AMO.sol
+++ contracts/SolidlyV2AMO.sol
@@ -124,7 +124,8 @@ contract SolidlyV2AMO is ISolidlyV2AMO, MasterAMO {
         uint256 boostSellRatio_,
         uint256 usdBuyRatio_
     ) public override onlyRole(SETTER_ROLE) {
-        if (validRangeRatio_ > FACTOR || validRemovingRatio_ > FACTOR) revert InvalidRatioValue(); // validRangeRatio is a few percentage points (scaled with factor). So it needs to be lower than 1 (scaled with FACTOR)
+        if (validRangeRatio_ > FACTOR || validRemovingRatio_ < FACTOR) revert InvalidRatioValue(); // validRangeRatio is a few percentage points (scaled with factor). So it needs to be lower than 1 (scaled with FACTOR)
+        // validRemovingRatio nedds to be greater than 1 (we remove more BOOST than USD otherwise the pool is balanced )
         boostMultiplier = boostMultiplier_;
         validRangeRatio = validRangeRatio_;
         validRemovingRatio = validRemovingRatio_;
