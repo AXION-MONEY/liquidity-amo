c047ee4e1ff4b57679115e4d2150b7ecf9c040e2
MiKO <39623615+MiKoronjoo@users.noreply.github.com>
2025-05-12 00:05:58 +0330

    Fix DoS issue for V2AMO

--- contracts/V2AMO.sol
+++ contracts/V2AMO.sol
@@ -241,10 +241,7 @@ contract V2AMO is IV2AMO, MasterAMO {
         // Approve router to spend ION
         IERC20(ionAddress).approve(routerAddress, ionAmount);
         // Calculate minimum expected USD output based on target price
-        uint256 minPairTokenAmountOut = scaleIonToPairTokenDecimals(ionAmountWithoutFee).mulDiv(
-            targetPrice,
-            SCALED_UNIT
-        );
+        uint256 minPairTokenAmountOut = scaleIonToPairTokenDecimals(ionAmount).mulDiv(targetPrice, SCALED_UNIT);
         uint256 preOperationPairTokenBalance = balanceOfToken(pairTokenAddress);
 
         uint256[] memory amounts;
